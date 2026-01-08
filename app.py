e0d1569a0dbd76b0e9180be41f21f55c754086a3
sushisk <okunishi@DESKTOP-HU3KDQ1.localdomain>
2026-01-08 13:48:54 +0900

    0

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import gradio as gr
 
+def greet(name):
+    return f"こんにちは、{name} さん！"
+
+demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+if __name__ == "__main__":
+    demo.launch()
